edc39c3eea58d6ac7eccc3c355700630f6603e74
Chris Seaman <christopherseaman1983@gmail.com>
2024-02-13 15:14:06 +0000

    update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactForm from "./components/ContactForm";
 export default function ContactPage() {
   return (
     <div>
-      <h2 className="text-center font-bold text-xl mb-2">Contact Us</h2>
+      <h2 className="text-center font-bold text-xl mb-2">Get in touch</h2>
       <p className="mb-8 max-w-[400px] mx-auto">Looking for a photographer or videographer? Then send us a message, we&apos;re always happy to discuss any projects and how we could be of service.</p>
       <ContactForm />
     </div>
