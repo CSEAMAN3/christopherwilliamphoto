c6c458d7f79378cfb82dd42ee090891d3e68ca2b
Chris Seaman <christopherseaman1983@gmail.com>
2024-02-13 15:33:53 +0000

    wrapped providers around children in layout.ts file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} relative`}>
         <Header />
-        {children}
+          <Providers>
+            {children}
+          </Providers>
         <Footer />
       </body>
     </html>
